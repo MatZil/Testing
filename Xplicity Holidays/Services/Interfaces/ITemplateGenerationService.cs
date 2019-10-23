336e061d6e01644b565131b188cffcbe9b5a7b52
Matas Vaitkevičius <vaitkevicius.matas@gmail.com>
2019-10-23 18:08:07 +0300

    Template generation service

---
+++ Xplicity Holidays/Services/Interfaces/ITemplateGenerationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xplicity_Holidays.Constants;
+using Xplicity_Holidays.Infrastructure.Database.Models;
+
+namespace Xplicity_Holidays.Services.Interfaces
+{
+    public interface ITemplateGenerationService
+    {
+        Task TemplateGeneration(int id, HolidayType holidayType, HolidayDocumentType holidayDocumentType);
+    }
+}
---
+++ Xplicity Holidays/Services/TemplateGenerationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Xplicity_Holidays.Constants;
+using Xplicity_Holidays.Infrastructure.Database.Models;
+using Xplicity_Holidays.Infrastructure.Repositories;
+using Xplicity_Holidays.Infrastructure.TemplateGeneration;
+using Xplicity_Holidays.Services.Interfaces;
+
+namespace Xplicity_Holidays.Services
+{
+    public class TemplateGenerationService : ITemplateGenerationService
+    {
+        private readonly ITemplateGeneration _templateGeneration;
+
+        public TemplateGenerationService(ITemplateGeneration templateGeneration)
+        {
+            _templateGeneration = templateGeneration;
+        }
+
+        public async Task TemplateGeneration(int id, HolidayType holidayType, HolidayDocumentType holidayDocumentType)
+        {
+            await _templateGeneration.GenerateFileByTemplate(id, holidayType, holidayDocumentType);
+        }
+    }
+}
