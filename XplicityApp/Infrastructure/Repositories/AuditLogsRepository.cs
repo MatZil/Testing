32532de71176eb5b577d437c446ff549086acc2d
daumenas <daumius@gmail.com>
2020-02-05 11:22:12 +0200

    Fix repository files

--- XplicityApp/Infrastructure/Repositories/AuditLogsRepository.cs
+++ XplicityApp/Infrastructure/Repositories/AuditLogsRepository.cs
@@ -9,46 +9,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace XplicityApp.Infrastructure.Repositories
 {
-    public class AuditLogsRepository : IAuditLogsRepository
+    public class AuditLogsRepository : RepositoryBase<AuditLog>
     {
-        protected readonly HolidayDbContext Context;
+        protected override DbSet<AuditLog> ItemSet { get; }
 
-        public async Task<int> Create(AuditLog entity)
+        public AuditLogsRepository(HolidayDbContext context) : base(context)
         {
-            Context.AuditLogs.Add(entity);
-            await Context.SaveChangesAsync();
-
-            return entity.Id;
-        }
-
-        public async Task<bool> Delete(AuditLog entity)
-        {
-            Context.AuditLogs.Remove(entity);
-            var changes = await Context.SaveChangesAsync();
-
-            return changes > 0;
-        }
-
-        public async Task<ICollection<AuditLog>> GetAll()
-        {
-            var auditLogs = await Context.AuditLogs.ToArrayAsync();
-
-            return auditLogs;
-        }
-
-        public async Task<AuditLog> GetById(int id)
-        {
-            var auditLog = await Context.AuditLogs.FindAsync(id);
-
-            return auditLog;
-        }
-
-        public async Task<bool> Update(AuditLog entity)
-        {
-            Context.AuditLogs.Attach(entity);
-            var changes = await Context.SaveChangesAsync();
-
-            return changes > 0;
+            ItemSet = context.AuditLogs;
         }
     }
 }
