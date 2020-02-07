95f33bc6d1b9c909402de3e5e11d0b7df09a573a
unknown <zilinskas.matas1999@gmail.com>
2020-02-07 11:00:15 +0200

    Frontend adapted

--- XplicityApp/ClientApp/src/app/models/requestholidays.ts
+++ XplicityApp/ClientApp/src/app/models/requestholidays.ts
@@ -4,7 +4,7 @@ export class Requestholidays {
     employeeId: number;
     type: HolidayType;
     fromInclusive: Date;
-    toExclusive: Date;
+    toInclusive: Date;
     overtimeDays: number;
     paid: boolean;
 }
