a6aa68cb6f9fd74690b0d5f0236accf7f387c95f
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-02-28 16:25:27 +0200

    Update migrations. Add data seed

---
+++ XplicityApp/Migrations/20200228142357_AddNotificationSettings.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XplicityApp.Migrations
+{
+    public partial class AddNotificationSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BroadcastOwnBirthday = table.Column<bool>(nullable: false),
+                    ReceiveBirthdayNotifications = table.Column<bool>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotificationSettings_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "NotificationSettings",
+                columns: new[] { "Id", "BroadcastOwnBirthday", "EmployeeId", "ReceiveBirthdayNotifications" },
+                values: new object[] { 1, true, 1, true });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationSettings_EmployeeId",
+                table: "NotificationSettings",
+                column: "EmployeeId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationSettings");
+        }
+    }
+}
