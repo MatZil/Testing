813e509dbc0ebce0d78d5f646d4af2b0d03f089e
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-02-28 15:51:58 +0200

    Models created

---
+++ XplicityApp/Infrastructure/Database/Models/NotificationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XplicityApp.Infrastructure.Database.Models
+{
+    public class NotificationSettings : BaseEntity
+    {
+        [Required]
+        public bool BroadcastOwnBirthday { get; set; }
+        [Required]
+        public bool ReceiveBirthdayNotifications { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
