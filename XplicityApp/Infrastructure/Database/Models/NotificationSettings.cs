db32e42d94c3c2bb664cff0b8317121b977d9347
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-02-28 16:11:36 +0200

    Migrations added

--- XplicityApp/Infrastructure/Database/Models/NotificationSettings.cs
+++ XplicityApp/Infrastructure/Database/Models/NotificationSettings.cs
@@ -1,12 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace XplicityApp.Infrastructure.Database.Models
 {
     public class NotificationSettings : BaseEntity
     {
         [Required]
+        [DefaultValue(true)]
         public bool BroadcastOwnBirthday { get; set; }
         [Required]
+        [DefaultValue(true)]
         public bool ReceiveBirthdayNotifications { get; set; }
         [Required]
         public int EmployeeId { get; set; }
---
+++ XplicityApp/Migrations/20200228140753_AddNotificationSettings.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XplicityApp.Migrations
+{
+    public partial class AddNotificationSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BroadcastOwnBirthday = table.Column<bool>(nullable: false),
+                    ReceiveBirthdayNotifications = table.Column<bool>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotificationSettings_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationSettings_EmployeeId",
+                table: "NotificationSettings",
+                column: "EmployeeId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationSettings");
+        }
+    }
+}
