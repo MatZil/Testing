6cb7ad8b607cc732630f0e1ec1ed1309973d2b3b
unknown <zilinskas.matas1999@gmail.com>
2020-03-03 13:35:59 +0200

    Added RejectionReason Migration

---
+++ XplicityApp/Migrations/20200303112759_HolidayRejectionReason.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XplicityApp.Migrations
+{
+    public partial class HolidayRejectionReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReason",
+                table: "Holidays",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionReason",
+                table: "Holidays");
+        }
+    }
+}
