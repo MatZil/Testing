222c6e438ef27f52cf8ab9caf72e996bdae29f1d
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-03-06 19:01:49 +0200

    Remove empty spaces

--- XplicityApp/ClientApp/src/app/services/notification-settings.service.ts
+++ XplicityApp/ClientApp/src/app/services/notification-settings.service.ts
@@ -19,6 +19,4 @@ export class NotificationSettingsService {
   updateNotificationSettings(userId: number, updateNotificationSettings: NotificationSettings) {
     return this.http.put(`${this.notificationSettingsApi}/${userId}`, updateNotificationSettings);
   }
-
-
 }
