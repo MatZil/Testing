91ff06eddaccc20b70bbb6ad8e41b3f80180b672
unknown <zilinskas.matas1999@gmail.com>
2020-03-12 10:05:17 +0200

    Removed leftover .spec.ts files

--- XplicityApp/ClientApp/src/app/models/email-template.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmailTemplate } from './email-template';
-
-describe('EmailTemplate', () => {
-  it('should create an instance', () => {
-    expect(new EmailTemplate()).toBeTruthy();
-  });
-});
