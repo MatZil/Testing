58bf0225db8cc3b26f422b42c62658fed3da6ec8
daumenas <daumius@gmail.com>
2020-03-30 10:40:33 +0300

    Merge branch 'master' of https://dev.azure.com/xplicity/XplicityApplication/_git/XplicityApplication
    # Conflicts:
    #	XplicityApp/Services/EmailService.cs

---
+++ XplicityApp/Migrations/20200318144511_Depreciation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XplicityApp.Migrations
+{
+    public partial class Depreciation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "InventoryItems",
+                newName: "OriginalPrice");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentPrice",
+                table: "InventoryItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentPrice",
+                table: "InventoryItems");
+
+            migrationBuilder.RenameColumn(
+                name: "OriginalPrice",
+                table: "InventoryItems",
+                newName: "Price");
+        }
+    }
+}
---
+++ XplicityApp/Services/BackgroundFunctions/BackgroundInventoryUpdater.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using XplicityApp.Infrastructure.Repositories;
+using XplicityApp.Services.BackgroundFunctions.Interfaces;
+using XplicityApp.Infrastructure.Database.Models;
+using XplicityApp.Infrastructure.Utils.Interfaces;
+
+namespace XplicityApp.Services.BackgroundFunctions
+{
+    public class BackgroundInventoryUpdater : IBackgroundInventoryUpdater
+    {
+        const int licenseCategoryId = 4;
+
+        private readonly IInventoryItemRepository _inventoryItemRepository;
+        private readonly IRepository<InventoryCategory> _inventoryCategoryRepository;
+        private readonly ITimeService _timeService;
+
+        public BackgroundInventoryUpdater(
+            IInventoryItemRepository inventoryItemRepository,
+            IRepository<InventoryCategory> inventoryCategoryRepository,
+            ITimeService timeService)
+        {
+            _inventoryItemRepository = inventoryItemRepository;
+            _inventoryCategoryRepository = inventoryCategoryRepository;
+            _timeService = timeService;
+        }
+
+        public async Task ApplyDepreciationToInventoryItems()
+        {
+            var allCurrentlyUsedInventoryItems = await _inventoryItemRepository.GetByStatus(false);
+            var inventoryItemsToDepreciate = allCurrentlyUsedInventoryItems.Where(item => item.InventoryCategoryId != licenseCategoryId);
+
+            foreach (var item in inventoryItemsToDepreciate)
+            {
+                var category = await _inventoryCategoryRepository.GetById(item.InventoryCategoryId);
+                var serviceLifeInYears = (int)category.Deprecation;
+                var serviceLifeInDays = _timeService.ConvertYearsToDays(serviceLifeInYears);
+                var dailyDepreciation = (item.OriginalPrice - 1) / serviceLifeInDays;
+
+                item.CurrentPrice -= (decimal)dailyDepreciation;
+            }
+        }
+    }
+}
