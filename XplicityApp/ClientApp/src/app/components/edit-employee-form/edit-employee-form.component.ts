012b6bb0bcd9158d61e16f3a5c1319f9c923899b
Giedre Pranauskyte <giedre9723@gmail.com>
2020-04-16 14:49:09 +0300

    fix pr 743

--- XplicityApp/ClientApp/src/app/components/edit-employee-form/edit-employee-form.component.ts
+++ XplicityApp/ClientApp/src/app/components/edit-employee-form/edit-employee-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, DebugElement } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EmployeeStatus } from 'src/app/models/employee-status.enum';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
