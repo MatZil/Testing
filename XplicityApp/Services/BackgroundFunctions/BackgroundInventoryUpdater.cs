53d1402b3f1774f338e86d6dd4616e35ec585b14
Giedre Pranauskyte <giedre9723@gmail.com>
2020-06-24 20:54:06 +0300

    833: Recalculated price is not being saved to database

--- XplicityApp/Services/BackgroundFunctions/BackgroundInventoryUpdater.cs
+++ XplicityApp/Services/BackgroundFunctions/BackgroundInventoryUpdater.cs
@@ -4,6 +4,8 @@ using XplicityApp.Infrastructure.Repositories;
 using XplicityApp.Services.BackgroundFunctions.Interfaces;
 using XplicityApp.Infrastructure.Database.Models;
 using XplicityApp.Infrastructure.Utils.Interfaces;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace XplicityApp.Services.BackgroundFunctions
 {
@@ -14,33 +16,48 @@ namespace XplicityApp.Services.BackgroundFunctions
         private readonly IInventoryItemRepository _inventoryItemRepository;
         private readonly IRepository<InventoryCategory> _inventoryCategoryRepository;
         private readonly ITimeService _timeService;
+        private readonly ILogger<BackgroundInventoryUpdater> _logger;
 
         public BackgroundInventoryUpdater(
             IInventoryItemRepository inventoryItemRepository,
             IRepository<InventoryCategory> inventoryCategoryRepository,
-            ITimeService timeService)
+            ITimeService timeService,
+            ILogger<BackgroundInventoryUpdater> logger)
         {
             _inventoryItemRepository = inventoryItemRepository;
             _inventoryCategoryRepository = inventoryCategoryRepository;
             _timeService = timeService;
+            _logger = logger;
         }
 
         public async Task ApplyDepreciationToInventoryItems()
         {
+            var currentTime = _timeService.GetCurrentTime();
+            _logger.LogInformation("ApplyDepreciationToInventoryItems() was initiated at " + currentTime);
+
             var allCurrentlyUsedInventoryItems = await _inventoryItemRepository.GetByStatus(false);
             var inventoryItemsToDepreciate = allCurrentlyUsedInventoryItems.Where(item => item.InventoryCategoryId != licenseCategoryId);
-
-            foreach (var item in inventoryItemsToDepreciate)
+            
+            try
             {
-                var category = await _inventoryCategoryRepository.GetById(item.InventoryCategoryId);
-                var serviceLifeInYears = (int)category.Deprecation;
-                var serviceLifeInDays = _timeService.ConvertYearsToDays(serviceLifeInYears);
-                var dailyDepreciation = (item.OriginalPrice - 1) / serviceLifeInDays;
+                foreach (var item in inventoryItemsToDepreciate)
+                {
+                    var category = await _inventoryCategoryRepository.GetById(item.InventoryCategoryId);
+                    var serviceLifeInYears = (int)category.Deprecation;
+                    var serviceLifeInDays = _timeService.ConvertYearsToDays(serviceLifeInYears);
+                    var dailyDepreciation = (item.OriginalPrice - 1) / serviceLifeInDays;
 
-                item.CurrentPrice -= (decimal)dailyDepreciation;
+                    item.CurrentPrice -= (decimal)dailyDepreciation;
 
-                await _inventoryItemRepository.Update(item);
+                    await _inventoryItemRepository.Update(item);
+                }
             }
+            catch (Exception exception)
+            {
+                _logger.LogInformation(exception.ToString() + " occurred in ApplyDepreciationToInventoryItems() at " + currentTime);
+            }
+
+        _logger.LogInformation("ApplyDepreciationToInventoryItems() ended at " + currentTime);
         }
     }
 }
