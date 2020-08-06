1fceca6d66b7a0f4433c413fd8f7f249cac85123
Andrius Tolvaišis <andrius@xplicity.com>
2020-08-06 23:11:31 +0300

    Removed dropping HolidayGuids table from migration.

--- XplicityApp/Migrations/20200621092055_Answers.cs
+++ XplicityApp/Migrations/20200621092055_Answers.cs
@@ -6,9 +6,6 @@ namespace XplicityApp.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "HolidayGuids");
-
             migrationBuilder.CreateTable(
                 name: "Answers",
                 columns: table => new
@@ -30,13 +27,6 @@ namespace XplicityApp.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.UpdateData(
-                table: "FileRecords",
-                keyColumn: "Id",
-                keyValue: 1,
-                column: "Guid",
-                value: "89882d8e-5395-445b-8163-cb5eeba3e7fe-43e00511-fb58-4448-8411-9098c1f6ed56");
-
             migrationBuilder.CreateIndex(
                 name: "IX_Answers_QuestionId",
                 table: "Answers",
@@ -47,40 +37,6 @@ namespace XplicityApp.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Answers");
-
-            migrationBuilder.CreateTable(
-                name: "HolidayGuids",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ConfirmerId = table.Column<int>(type: "int", nullable: false),
-                    Guid = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    HolidayId = table.Column<int>(type: "int", nullable: false),
-                    IsAdmin = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_HolidayGuids", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_HolidayGuids_Holidays_HolidayId",
-                        column: x => x.HolidayId,
-                        principalTable: "Holidays",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.UpdateData(
-                table: "FileRecords",
-                keyColumn: "Id",
-                keyValue: 1,
-                column: "Guid",
-                value: "3eb1d829-f892-4108-aefd-54aaa7789ad8");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_HolidayGuids_HolidayId",
-                table: "HolidayGuids",
-                column: "HolidayId");
         }
     }
 }
