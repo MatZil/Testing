1b0fcafe95e4a11a30abc94cb23a3f447e00c55b
Margir <martynas.girdziuna@gmail.com>
2020-11-08 14:41:07 +0200

    Tests for holidays and inventory test

---
+++ Tests/Tests/Background/InventoryUpdaterTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using XplicityApp.Infrastructure.Repositories;
+using Xunit;
+using XplicityApp.Services.BackgroundFunctions;
+using System.Linq;
+using XplicityApp.Infrastructure.Utils;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Tests.Tests.BackgroundTests
+{
+    [TestCaseOrderer("Tests.InventoryUpdaterTests.AlphabeticalOrderer", "Tests")]
+    public class InventoryUpdaterTests
+    {
+        const int licenseCategoryId = 4;
+
+        private readonly InventoryItemsRepository _inventoryItemRepository;
+        private readonly BackgroundInventoryUpdater _backgroundInventoryUpdater;
+        private readonly InventoryCategoryRepository _inventoryCategoryRepository;
+        private readonly TimeService _timeService;
+
+        public InventoryUpdaterTests()
+        {
+            var setup = new SetUp();
+            setup.Initialize();
+            var context = setup.HolidayDbContext;
+
+            _inventoryItemRepository = new InventoryItemsRepository(context);
+            _inventoryCategoryRepository = new InventoryCategoryRepository(context);
+            _timeService = new TimeService();
+            var _logger = new Mock<ILogger<BackgroundInventoryUpdater>>().Object;
+
+            _backgroundInventoryUpdater = new BackgroundInventoryUpdater(_inventoryItemRepository, _inventoryCategoryRepository, _timeService, _logger);
+        }
+
+        [Fact]
+        public async void When_DepreciatingInventoryItems_Expect_CurrentPriceDepreciated()
+        {
+            var allInventoryItems = await _inventoryItemRepository.GetAll();
+            var formerPrices = allInventoryItems.Where(item => item.InventoryCategoryId != licenseCategoryId)
+                                                 .Select(item => item.CurrentPrice).ToList();
+
+            await _backgroundInventoryUpdater.ApplyDepreciationToInventoryItems();
+
+            var allUpdatedInventoryItems = await _inventoryItemRepository.GetAll();
+            var tangibleItems = allUpdatedInventoryItems.Where(item => item.InventoryCategoryId != licenseCategoryId);
+
+            for (int i = 0; i < tangibleItems.Count(); i++)
+            {
+                var currentPrice = tangibleItems.ElementAt(i).CurrentPrice;
+                var formerPrice = formerPrices.ElementAt(i);
+                Assert.True(currentPrice < formerPrice, "Failed to depreciate all tangible items.");
+            }
+        }
+
+        [Fact]
+        public async void When_DepreciatingLicenses_Expect_NoDepreciationApplied()
+        {
+            var allInventoryItems = await _inventoryItemRepository.GetAll();
+            var formerPrices = allInventoryItems.Where(item => item.InventoryCategoryId == licenseCategoryId)
+                                                 .Select(item => item.CurrentPrice).ToList();
+
+            await _backgroundInventoryUpdater.ApplyDepreciationToInventoryItems();
+
+            var allUpdatedInventoryItems = await _inventoryItemRepository.GetAll();
+            var licenses = allUpdatedInventoryItems.Where(item => item.InventoryCategoryId == licenseCategoryId);
+
+            for (int i = 0; i < licenses.Count(); i++)
+            {
+                var currentPrice = licenses.ElementAt(i).CurrentPrice;
+                var formerPrice = formerPrices.ElementAt(i);
+                Assert.True(currentPrice == formerPrice, "Item of 'Software license' category was depreciated.");
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void When_FullDepreciationIsApplied_Expect_CurrentPriceEqualsOne(int id)
+        {
+            var item = await _inventoryItemRepository.GetById(id);
+            var category = await _inventoryCategoryRepository.GetById(item.InventoryCategoryId);
+            var lifeExpectancyInDays = _timeService.ConvertYearsToDays((int)category.Deprecation);
+
+            for (int i = 0; i < lifeExpectancyInDays; i++)
+                await _backgroundInventoryUpdater.ApplyDepreciationToInventoryItems();
+
+            var itemAfterFullDepreciation = await _inventoryItemRepository.GetById(id);
+            var priceAfterFullDepreciation = Math.Round(itemAfterFullDepreciation.CurrentPrice);
+
+            Assert.True(priceAfterFullDepreciation == 1, "Failed to apply full depreciation.");
+        }
+
+        [Theory]
+        [InlineData(1, 30, 92)]
+        [InlineData(1, 90, 76)]
+        [InlineData(1, 180, 51)]
+        public async void When_PartialDepreciationIsApplied_Expect_CurrentPriceIsChanged(int id, int deprecationTimeInDays, decimal expectedPrice)
+        {
+            for (int i = 0; i < deprecationTimeInDays; i++)
+                await _backgroundInventoryUpdater.ApplyDepreciationToInventoryItems();
+
+            var priceAfterDepreciation = Math.Round((await _inventoryItemRepository.GetById(id)).CurrentPrice);
+
+            Assert.True(priceAfterDepreciation == expectedPrice);
+        }
+    }
+}
+
---
+++ Tests/Tests/Holidays/HolidayTests.cs
@@ -0,0 +1,136 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Moq;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using XplicityApp.Configurations;
+using XplicityApp.Infrastructure.Database;
+using XplicityApp.Infrastructure.Enums;
+using XplicityApp.Infrastructure.Repositories;
+using XplicityApp.Infrastructure.Utils;
+using XplicityApp.Infrastructure.Utils.Interfaces;
+using XplicityApp.Services;
+using XplicityApp.Services.Interfaces;
+using Xunit;
+using Xunit.Abstractions;
+using XplicityApp.Dtos.Holidays;
+
+namespace Tests.Tests.Holidays
+{
+    public class HolidayTests
+    {
+        private readonly HolidayDbContext _context;
+        private readonly int _holidaysCount;
+        private readonly ITestOutputHelper _output;
+        private readonly HolidaysService _holidaysService;
+        private readonly EmployeesService _employeesService;
+        private readonly EmployeesRepository _employeesRepository;
+        private readonly HolidaysRepository _holidaysRepository;
+        private readonly ClientsRepository _clientsRepository;
+        private readonly IConfiguration _configuration;
+        private readonly TimeService _timeService;
+
+        public HolidayTests(ITestOutputHelper output)
+        {
+            _output = output;
+            var setup = new SetUp();
+            setup.Initialize();
+            _context = setup.HolidayDbContext;
+            var mapper = setup.Mapper;
+            var userManager = setup.InitializeUserManager();
+            _holidaysCount = setup.GetCount("holidays");
+            _configuration = setup.GetConfiguration();
+            _timeService = new TimeService();
+            var mockUserService = new Mock<IUserService>().Object;
+            var mockOvertimeUtility = new Mock<IOvertimeUtility>().Object;
+            _holidaysRepository = new HolidaysRepository(_context);
+            _employeesRepository = new EmployeesRepository(_context, userManager);
+            _clientsRepository = new ClientsRepository(_context);
+            var holidayGuidsRepository = new HolidayGuidsRepository(_context);
+            var mockNotificationSettingsService = new Mock<INotificationSettingsService>().Object;
+            _employeesService = new EmployeesService(_employeesRepository, mapper, mockOvertimeUtility, _timeService,
+                                                     mockUserService, mockNotificationSettingsService, _configuration);
+            _holidaysService = new HolidaysService(_holidaysRepository, _employeesRepository, mapper, _timeService,
+                                                   mockOvertimeUtility, _clientsRepository, mockUserService, _configuration, holidayGuidsRepository);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void When_CreatingHoliday_Expect_ReturnsCreatedHoliday(int employeeId)
+        {
+            var newHoliday = new NewHolidayDto()
+            {
+                EmployeeId = employeeId,
+                Type = HolidayType.DayForChildren,
+                FromInclusive = new DateTime(2019, 10, 24),
+                ToInclusive = new DateTime(2019, 10, 27),
+            };
+
+            var createdHolidayId = await _holidaysService.Create(newHoliday);
+            var createdHoliday = _context.Holidays.Find(createdHolidayId);
+
+            Assert.True(createdHolidayId > -1 && createdHoliday != null);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void When_GettingHolidayEmployeeFullName_Expect_ReturnedEmployeeFullNameIsCorrect(int id)
+        {
+            var retrievedHoliday = await _holidaysService.GetById(id);
+            var retrievedEmployee = await _employeesService.GetById(retrievedHoliday.EmployeeId);
+
+            var fullNameExpected = $"{retrievedEmployee.Name} {retrievedEmployee.Surname}";
+            var fullNameActual = retrievedHoliday.EmployeeFullName;
+
+            Assert.True(fullNameExpected.Equals(fullNameActual), "Employee's full name is incorrect.");
+        }
+
+        [Fact]
+        public void When_GetPublicHolidays_Expect_DaysCountWithoutHolidays()
+        {
+            var timeService = new TimeService();
+            var retrievedHolidays = timeService.GetWorkDays(new DateTime(2020, 12, 23), new DateTime(2020, 12, 27));
+
+            Assert.Equal(1, retrievedHolidays);
+        }
+
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(1, false)]
+        public void When_PublicHolidaysIsUnpaid_Expect_ThisDayIsUnpaid(int id, bool expected)
+        {
+            var result = _employeesService.HasActiveUnpaidHoliday(id);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void When_DeletingHoliday_Expect_True(int id)
+        {
+            var wasFound = false;
+            var wasDeleted = false;
+
+            var found = _context.Holidays.Find(id);
+            if (found != null) wasFound = true;
+
+            bool deletedHoliday = await _holidaysService.Delete(id);
+
+            found = _context.Holidays.Find(id);
+
+            if (found == null && deletedHoliday) wasDeleted = true;
+
+            Assert.True(wasFound && wasDeleted);
+        }
+    }
+}
+
